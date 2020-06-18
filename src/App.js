de5aec148f1aa3f914d0a3deb349506e77b2a994
Damian Isaac <damianisaacalexander@gmail.com>
2020-06-18 12:32:19 -0500

    changed react reducer to incorop search sort

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useReducer, useEffect} from "react";
+import sortReducer from "./utils/sortReducer";
+import API from "./utils/API";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
+import Footer from "./components/Footer/Footer";
+import Table from "./components/Table/Table";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const initalState = {employees: []};
+    const [state, dispatch] = useReducer(sortReducer, initalState);
+    const [searchVal, setSearchVal] = useState("");
+
+    useEffect(() => {
+        async function loadEmployeeList(){
+            const result = await API.getEmployees();
+            dispatch({type: "setEmployeeList", payLoad: result.data.results});
+        }
+        loadEmployeeList();
+    }, []);
+
+    useEffect(()=> {
+        dispatch({type: "find", input: searchVal});
+    }, [searchVal])
+
+    return (
+        <Wrapper>
+            <Header>
+                <Search dispatch={dispatch} setSearchVal={setSearchVal} searchVal={searchVal}/>
+            </Header>
+            <Table state={state} dispatch={dispatch}/>
+            <Footer />
+        </Wrapper>
+    );
 }
 
 export default App;
